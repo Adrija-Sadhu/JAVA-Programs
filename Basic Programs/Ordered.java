a8058f754b0c2fa2cb51fc73f5a26eb4336e807b
Adrija Sadhu <adrijasadhu2724@gmail.com>
2025-03-20 16:03:01 +0530

    Initial Commit

---
+++ Basic Programs/Ordered.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Ordered {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int n;
+		System.out.println("Enter a number : ");
+		n=sc.nextInt();
+		
+		int a,flag=1,b=9;
+		while(n!=0)
+		{
+			a=n%10;
+			if(a>b)
+			{
+				flag=0;
+				break;
+			}
+				b=a;
+			n/=10;
+		}
+		if(flag==0)
+			System.out.println("Not well ordered");
+		else
+			System.out.println("Well ordered");
+	}
+
+}
