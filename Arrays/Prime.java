8aa77ca1da6abc727451e1b8a539f9d233b9ea04
Adrija Sadhu <adrijasadhu2724@gmail.com>
2025-03-21 21:27:04 +0530

    Generate first 100 prime numbers

---
+++ Arrays/Prime.java
@@ -0,0 +1,29 @@
+// Generate first 100 prime numbers 
+public class Prime {
+    public static boolean isPrime(int n)
+        {
+            boolean flag=true;
+            for(int i=0;i<n;i++)
+            {
+                if(n%i==0)
+                flag=false;
+            }
+            return flag;
+        }
+        public static void main(String[] args)
+        {
+            int[] arr=new int[100];
+    
+            int j=2;
+    
+            for(int i=0;i<100;i++)
+            {
+                if(isPrime(j))
+            arr[i++]=j;
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
