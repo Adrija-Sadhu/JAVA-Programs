f856f950f29791ca6e6d4b4e9b9926f8a28dbc44
Adrija Sadhu <adrijasadhu2724@gmail.com>
2025-03-22 11:40:09 +0530

    Initial Commit

---
+++ HashMap Programs/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+//Remove DUplicate elements from an integer array using HashMap
+import java.util.*;
+public class RemoveDuplicates {
+
+    public static void main(String[] args)
+    {
+        int[] arr={10,20,20,21,25,26,28,28,28};
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i:arr)
+        {
+            if(!map.containsKey(i))
+            {
+                map.put(i,1);
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
