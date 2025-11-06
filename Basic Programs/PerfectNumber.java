e83045805bd4962d3afdb2b480db4cc64a29d822
Adrija Sadhu <adrijasadhu2724@gmail.com>
2025-11-06 20:37:47 +0530

    Perfect Number

---
+++ Basic Programs/PerfectNumber.java
@@ -0,0 +1,27 @@
+/* Check if a number is perfect number or not
+ * A number is a perfect number if its sum of digits is equal to the number 
+ */
+import java.util.*;
+public class PerfectNumber {
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        int sum=0;
+
+        for(int i=1;i<n;i++){
+            if(n%i==0)
+            sum+=i;
+        }
+
+        if(sum==n)
+        System.out.println("True");
+        else
+        System.out.println("False");
+
+        sc.close();
+
+    }
+}
